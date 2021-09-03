a756f96043b6d275a7d5f2638a6586f8d9529a11
Jacob Hildebrandt <jacob@commerce7.com>
2021-09-03 14:03:20 -0700

    Setup Connection to MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env" });
+
+const DB = process.env.DB_CONNECT_STRING.replace(
+  "password",
+  process.env.DB_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connection successful!"))
+  .catch((err) => console.log(err));
 
 const server = new ApolloServer({
   typeDefs,
